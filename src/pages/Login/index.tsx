2c8dfb8805d1f3c25e67809b65ae4ff37698d2da
lucasafonsobastos <lucasafonsob@hotmail.com>
2023-08-16 11:29:54 -0300

    inicio

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,66 @@
+import { useForm } from "react-hook-form";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+import { Container, LoginContainer, Column, Spacing, Title } from "./styles";
+import { defaultValues, IFormLogin } from "./types";
+
+const schema = yup
+  .object({
+    email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
+    password: yup
+      .string()
+      .min(6, "No minimo 6 caracteres")
+      .required("Campo obrigatório"),
+  })
+  .required();
+
+const Login = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<IFormLogin>({
+    resolver: yupResolver(schema),
+    mode: "onBlur",
+    defaultValues,
+    reValidateMode: "onChange",
+  });
+
+  const onSubmit = (data: IFormLogin) => {
+    // Lidar com os dados do formulário quando ele for válido
+    alert('Formulário validado com sucesso! ');
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <LoginContainer>
+        <Column>
+          <Title>Login</Title>
+          <Spacing />
+          <Input
+            name="email"
+            placeholder="Email"
+            control={control}
+            errorMessage={errors?.email?.message}
+          />
+          <Spacing />
+          <Input
+            name="password"
+            type="password"
+            placeholder="Senha"
+            control={control}
+            errorMessage={errors?.password?.message}
+          />
+          <Spacing />
+          <Button title="Entrar" disabled={!isValid} onClick={handleSubmit(onSubmit)}/>
+        </Column>
+      </LoginContainer>
+    </Container>
+  );
+};
+
+export default Login;
